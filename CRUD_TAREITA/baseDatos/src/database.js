d1bcdd3c726de7ff48fe6a5fcd367b09faf964f9
karolm0406 <kherreram2200@alumno.ipn.mx>
2025-04-07 19:52:20 -0600

    tarea

--- CRUD_TAREITA/baseDatos/src/database.js
+++ CRUD_TAREITA/baseDatos/src/database.js
@@ -3,7 +3,7 @@ import { createPool } from 'mysql2/promise';
 const pool = createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Regina.0406',  
+    password: 'n0m3l0',  
     database: 'kdramas_nuevo_db' 
 });
 
